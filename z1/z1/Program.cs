78a3ff73988a42ddd09ab4d8a064fb1d40567ca7
AnnaIvanova111 <43719830+AnnaIvanova111@users.noreply.github.com>
2018-10-12 10:21:04 +0300

    Add files via upload

---
+++ z1/z1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 18.09.2018
+ * Time: 15:43
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace z1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			Console.WriteLine("Hello Depression");
+	           Console.WriteLine("Hello");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
